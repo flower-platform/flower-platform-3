57c491da94f3ab33a86a251c185266dc04c07e57
mariana.gheorghe <mariana.gheorghe@crispico.com>
2013-07-17 15:38:54 +0300

    Editor.Model: CodeSyncElement processor

--- org.flowerplatform.codesync.code.java/src/org/flowerplatform/editor/model/java/JavaClassProcessor.java
+++ org.flowerplatform.codesync.code.java/src/org/flowerplatform/editor/model/java/JavaClassProcessor.java
@@ -1,78 +1,70 @@
 package org.flowerplatform.editor.model.java;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.change.FeatureChange;
-import org.flowerplatform.editor.model.change_processor.IDiagrammableElementFeatureChangesProcessor;
+import org.flowerplatform.codesync.remote.CodeSyncElementFeatureChangesProcessor;
 import org.flowerplatform.editor.model.java.remote.JavaClassDiagramOperationsService;
 import org.flowerplatform.emf_model.notation.Node;
 import org.flowerplatform.emf_model.notation.NotationFactory;
 import org.flowerplatform.emf_model.notation.View;
 
+import com.crispico.flower.mp.codesync.code.java.adapter.JavaAttributeModelAdapter;
 import com.crispico.flower.mp.model.codesync.CodeSyncElement;
-import com.crispico.flower.mp.model.codesync.CodeSyncPackage;
 
 /**
  * @author Mariana
  */
-public class JavaClassProcessor implements IDiagrammableElementFeatureChangesProcessor {
+public class JavaClassProcessor extends CodeSyncElementFeatureChangesProcessor {
 
-	@Override
-	public void processFeatureChanges(EObject object, List<FeatureChange> featureChanges, View associatedViewOnOpenDiagram, Map<String, Object> context) {
-		if (object == null) {
-			return;
-		}
-		
-		List<Object> objectsToUpdate = new ArrayList<Object>();
-		
-		if (featureChanges == null) {
-			// full content
-			processFeatureChange(object, null, associatedViewOnOpenDiagram, context, objectsToUpdate);
-		} else {
-			objectsToUpdate.add(associatedViewOnOpenDiagram);
-			for (FeatureChange featureChange : featureChanges) {
-				processFeatureChange(object, featureChange, associatedViewOnOpenDiagram, context, objectsToUpdate);
-			}
-		}
-	}
-
-	protected void processFeatureChange(EObject object, FeatureChange featureChange, View associatedViewOnOpenDiagram, Map<String, Object> context, List<Object> objectsToUpdate) {
-		if (featureChange == null) {
-			processAttributes(object, associatedViewOnOpenDiagram, context, objectsToUpdate);
-		} else {
-			if (CodeSyncPackage.eINSTANCE.getCodeSyncElement_Children().equals(featureChange.getFeature())) {
-				processAttributes(object, associatedViewOnOpenDiagram, context, objectsToUpdate);
-			}
-		}
+	protected void processChildren(EObject object, List<EObject> childModelElements, View associatedViewOnOpenDiagram, List<Node> childViews, Map<String, Object> context) {
+		// attributes
+		super.processChildren(object, filterChildModelElements(childModelElements, JavaAttributeModelAdapter.ATTRIBUTE), associatedViewOnOpenDiagram, filterChildViews(childViews, "classAttribute"), context);
 	}
 	
-	private void processAttributes(EObject object, View associatedViewOnOpenDiagram, Map<String, Object> context, List<Object> objectsToUpdate) {
-		int attributeSeparatorIndex = getAttributeSeparatorIndex(associatedViewOnOpenDiagram);
-		if (attributeSeparatorIndex >= 0) {
-			// if there is an attribute separator => add views for all the attributes under the separator
-			for (EObject child : getCodeSyncElement(object).getChildren()) {
-				attributeSeparatorIndex++;
-				if (!contains(associatedViewOnOpenDiagram, child)) {
-					Node node = NotationFactory.eINSTANCE.createNode();
-//					node.setViewType(getCodeSyncElement(child).getType());
-					node.setViewType("classAttribute");
-					node.setDiagrammableElement(child);
-					associatedViewOnOpenDiagram.getPersistentChildren().add(attributeSeparatorIndex, node);
-				}
-			}
-		} else {
-			// no attribute separator => remove all the view associated to attributes
-			for (Iterator iterator = associatedViewOnOpenDiagram.getPersistentChildren().iterator(); iterator.hasNext();) {
-				View view = (View) iterator.next();
-				if (view.getViewType().equals("classAttribute")) {
-					iterator.remove();
-				}
+	protected List<EObject> filterChildModelElements(List<EObject> list, String filter) {
+		List<EObject> result = new ArrayList<EObject>();
+		for (EObject child : list) {
+			if (filter.equals(((CodeSyncElement) child).getType())) {
+				result.add(child);
 			}
 		}
+		return result;
+	}
+	
+	protected List<Node> filterChildViews(List<Node> list, String filter) {
+		List<Node> result = new ArrayList<Node>();
+		for (Node child : list) {
+			if (filter.equals(child.getViewType())) {
+				result.add(child);
+			}
+		}
+		return result;
+	}
+	
+	@Override
+	protected int getNewViewsIndex(EObject object, View associatedViewOnOpenDiagram) {
+		int index = getAttributeSeparatorIndex(associatedViewOnOpenDiagram);
+		if (index == -1) {
+			return index;
+		}
+		return ++index;
+	}
+
+	@Override
+	protected Node createChildView(View associatedViewOnOpenDiagram, EObject child) {
+		Node node = NotationFactory.eINSTANCE.createNode();
+//		node.setViewType(getCodeSyncElement(child).getType());
+		node.setViewType("classAttribute");
+		return node;
+	}
+
+	@Override
+	protected CodeSyncElement createModelElementChild(EObject object, View child) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	protected int getAttributeSeparatorIndex(View view) {
@@ -82,21 +74,6 @@ public class JavaClassProcessor implements IDiagrammableElementFeatureChangesPro
 			}
 		}
 		return -1;
-	}
-	
-	protected boolean contains(View view, EObject candidate) {
-		String candidateFragment = candidate.eResource().getURIFragment(candidate);
-		for (Node node : view.getPersistentChildren()) {
-			EObject child = node.getDiagrammableElement();
-			if (child != null && child.eResource().getURIFragment(child).equals(candidateFragment)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	protected CodeSyncElement getCodeSyncElement(EObject object) { 
-		return (CodeSyncElement) object;
 	}
 	
 }
---
+++ org.flowerplatform.codesync/src/org/flowerplatform/codesync/remote/CodeSyncElementFeatureChangesProcessor.java
@@ -0,0 +1,134 @@
+package org.flowerplatform.codesync.remote;
+
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.change.FeatureChange;
+import org.flowerplatform.editor.model.change_processor.IDiagrammableElementFeatureChangesProcessor;
+import org.flowerplatform.emf_model.notation.Node;
+import org.flowerplatform.emf_model.notation.View;
+
+import com.crispico.flower.mp.codesync.base.CodeSyncPlugin;
+import com.crispico.flower.mp.model.codesync.CodeSyncElement;
+import com.crispico.flower.mp.model.codesync.CodeSyncPackage;
+
+/**
+ * @author Mariana Gheorghe
+ */
+public abstract class CodeSyncElementFeatureChangesProcessor implements IDiagrammableElementFeatureChangesProcessor {
+
+	@Override
+	public void processFeatureChanges(EObject object, List<FeatureChange> featureChanges, View associatedViewOnOpenDiagram, Map<String, Object> context) {
+		if (object == null) {
+			return;
+		}
+		
+		if (featureChanges == null) {
+			// full content
+			processFeatureChange(object, null, associatedViewOnOpenDiagram, context);
+		} else {
+			for (FeatureChange featureChange : featureChanges) {
+				processFeatureChange(object, featureChange, associatedViewOnOpenDiagram, context);
+			}
+		}
+	}
+	
+	protected void processFeatureChange(EObject object, FeatureChange featureChange, View associatedViewOnOpenDiagram, Map<String, Object> context) {
+		if (featureChange == null) {
+			processChildren(object, getChildrenForCodeSyncElement(object), associatedViewOnOpenDiagram, getChildrenForView(associatedViewOnOpenDiagram), context);
+		} else {
+			if (CodeSyncPackage.eINSTANCE.getCodeSyncElement_Children().equals(featureChange.getFeature())) {
+				processChildren(object, getChildrenForCodeSyncElement(object), associatedViewOnOpenDiagram, getChildrenForView(associatedViewOnOpenDiagram), context);
+			}
+		}
+	}
+
+	protected void processChildren(EObject object, List<EObject> childModelElements, View associatedViewOnOpenDiagram, List<Node> childViews, Map<String, Object> context) {
+		int newViewsIndex = getNewViewsIndex(object, associatedViewOnOpenDiagram);
+		
+		if (newViewsIndex >= 0) {
+			// add a view for each child of the model element (cases: initial, or adding a new child model element)
+			for (EObject child : childModelElements) {
+				if (!containsChildViewForModelElement(associatedViewOnOpenDiagram, child)) {
+					Node newView = createChildView(associatedViewOnOpenDiagram, child);
+					newView.setDiagrammableElement(child);
+					associatedViewOnOpenDiagram.getPersistentChildren().add(newViewsIndex, newView);
+					newViewsIndex++;
+				}
+			}
+		}
+		
+		// add a model element for each view (cases: add a new view on client side)
+		for (View child : childViews) {
+			if (!containsModelElementForChildView(object, child)) {
+				CodeSyncElement newChild = createModelElementChild(object, child);
+				if (newChild != null) {
+					getChildrenForCodeSyncElement(object).add(associatedViewOnOpenDiagram.getPersistentChildren().indexOf(child), newChild);
+				}
+			}
+		}
+		
+		if (newViewsIndex < 0) {
+			// remove all views (cases: collapsed view/compartment)
+			associatedViewOnOpenDiagram.getPersistentChildren().removeAll(childViews);
+		}
+	}
+	
+	protected CodeSyncElement getCodeSyncElement(EObject object) { 
+		return (CodeSyncElement) object;
+	}
+	
+	protected List<EObject> getChildrenForCodeSyncElement(EObject object) {
+		return (List<EObject>) 
+				CodeSyncPlugin.getInstance().getFeatureValue(getCodeSyncElement(object), CodeSyncPackage.eINSTANCE.getCodeSyncElement_Children());
+	}
+	
+	protected List<Node> getChildrenForView(View view) {
+		return view.getPersistentChildren();
+	}
+	
+	//////////////////////////////////////
+	// Model elements to views methods
+	//////////////////////////////////////
+	
+	abstract protected int getNewViewsIndex(EObject object, View associatedViewOnOpenDiagram);
+	
+	abstract protected Node createChildView(View associatedViewOnOpenDiagram, EObject child);
+	
+	protected boolean containsChildViewForModelElement(View view, EObject candidate) {
+		String candidateFragment = candidate.eResource().getURIFragment(candidate);
+		for (Node node : view.getPersistentChildren()) {
+			EObject child = node.getDiagrammableElement();
+			if (child != null && child.eResource().getURIFragment(child).equals(candidateFragment)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//////////////////////////////////////
+	// Views to model elements methods
+	//////////////////////////////////////
+		
+	abstract protected CodeSyncElement createModelElementChild(EObject object, View child);
+	
+	protected boolean containsModelElementForChildView(EObject object, View candidate) {
+		EObject diagrammableElement = candidate.getDiagrammableElement();
+		if (diagrammableElement == null) {
+			return false;
+		}
+		String candidateFragment =diagrammableElement.eResource().getURIFragment(diagrammableElement);
+		if (candidateFragment.equals(object.eResource().getURIFragment(object))) {
+			return true;
+		}
+		List<EObject> children = getChildrenForCodeSyncElement(object);
+		for (EObject child : children) {
+			if (candidateFragment.equals(child.eResource().getURIFragment(child))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
