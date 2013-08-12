1f919064942a6857977fbd349b66b2b5f5fd747f
Victor Badila <victor.badila@crispico.com>
2013-08-12 16:53:26 +0300

    12 august 2013

--- org.flowerplatform.web.svn/src/org/flowerplatform/web/svn/SvnPlugin.java
+++ org.flowerplatform.web.svn/src/org/flowerplatform/web/svn/SvnPlugin.java
@@ -9,29 +9,45 @@ import org.flowerplatform.communication.tree.remote.GenericTreeStatefulService;
 import org.osgi.framework.BundleContext;
 
 /**
+ * 
+ * @author Victor Badila
+ * 
  * @flowerModelElementId _GO_RkP2kEeKrJqcAep-lCg
  */
 public class SvnPlugin extends AbstractFlowerJavaPlugin {
 	/**
 	 * @flowerModelElementId _1w_YoP2kEeKrJqcAep-lCg
 	 */
-	private SvnPlugin INSTANCE;
+	protected static SvnPlugin INSTANCE;
 	/**
 	 * @flowerModelElementId _sK_EsP3FEeKrJqcAep-lCg
 	 */
 	private List<GenericTreeStatefulService> treeStatefulServicesDisplayingSvnContent = new ArrayList<GenericTreeStatefulService>();;
-
+	
+	public List<GenericTreeStatefulService> getTreeStatefulServicesDisplayingSvnContent() {
+		return treeStatefulServicesDisplayingSvnContent;
+	}
+	
+	public static SvnPlugin getInstance() {		
+		return INSTANCE;		
+	}
+	
 	/**
 	 * @flowerModelElementId _fhKA8P3GEeKrJqcAep-lCg
 	 */
-	public void start(BundleContext context) {
-		// TODO implement
+	
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		
+		INSTANCE = this;		
 	}
-
+	
 	/**
 	 * @flowerModelElementId _wPIaIP3GEeKrJqcAep-lCg
 	 */
-	public void stop(BundleContext context) {
-		// TODO implement
+	public void stop(BundleContext context) throws Exception{
+		super.stop(context);
+		
+		INSTANCE = null;	
 	}
 }
--- org.flowerplatform.web.svn/src/org/flowerplatform/web/svn/explorer/SvnRepository_SvnRepositoriesChildrenProvider.java
+++ org.flowerplatform.web.svn/src/org/flowerplatform/web/svn/explorer/SvnRepository_SvnRepositoriesChildrenProvider.java
@@ -1,14 +1,22 @@
 package org.flowerplatform.web.svn.explorer;
 
-
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.flowerplatform.common.util.Pair;
 import org.flowerplatform.communication.tree.GenericTreeContext;
 import org.flowerplatform.communication.tree.IChildrenProvider;
 import org.flowerplatform.communication.tree.remote.TreeNode;
+import org.flowerplatform.web.svn.SvnNodeType;
+import org.tigris.subversion.subclipse.core.SVNException;
+import org.tigris.subversion.subclipse.core.repo.SVNRepositoryLocation;
 
 /**
+ * * Parent node = Svn Repositories virtual node (i.e. Pair<File, String>).<br/>
+ * Child node = svn repository
+ * 
+ * @author Victor Badila
+ * 
  * @flowerModelElementId _UcYSoP3LEeKrJqcAep-lCg
  */
 public class SvnRepository_SvnRepositoriesChildrenProvider implements
@@ -16,39 +24,42 @@ public class SvnRepository_SvnRepositoriesChildrenProvider implements
 	/**
 	 * Intr-o prima faza, hardcodam la svn://csp1/flower2. Copiii sunt string
 	 * 
-	 * Apoi, se va lua din BD: get all svn repos for organization = crt. Copiii sunt SvnRepository
+	 * Apoi, se va lua din BD: get all svn repos for organization = crt. Copiii
+	 * sunt SvnRepository
 	 * 
 	 * @flowerModelElementId _kWPuUP3LEeKrJqcAep-lCg
 	 */
-//	@Override
-//	public Collection<Pair<Object, String>> getChildrenForNode(Object node,
-//			String treeNode, GenericTreeContext context) {
-//		// TODO implement
-//		return null;
-//	}
-	
+
 	@Override
 	public Collection<Pair<Object, String>> getChildrenForNode(Object node,
 			TreeNode treeNode, GenericTreeContext context) {
-		// TODO implement
+
+		Collection<Pair<Object, String>> result = new ArrayList<Pair<Object, String>>();
+
+		try {
+			SVNRepositoryLocation rootLocation = SVNRepositoryLocation
+					.fromString("svn://csp1/flower2");
+			rootLocation.setLabel("svn://csp1/flower2");
+			Pair<Object, String> hardcodedSvn = new Pair<Object, String>(
+					rootLocation, SvnNodeType.NODE_TYPE_REPOSITORY);
+			result.add(hardcodedSvn);
+			return result;
+		} catch (SVNException e) {
+			e.printStackTrace();
+		}
+
 		return null;
+
 	}
 
 	/**
 	 * @flowerModelElementId _z6dDEP3LEeKrJqcAep-lCg
 	 */
-//	@Override
-//	public Boolean nodeHasChildren(Object node, String treeNode,
-//			GenericTreeContext context) {
-//		// TODO implement
-//		return false;
-//	}
-	
+
 	@Override
 	public Boolean nodeHasChildren(Object node, TreeNode treeNode,
 			GenericTreeContext context) {
-		// TODO implement
-		return false;
+		return true; // momentan
 	}
 
 }
