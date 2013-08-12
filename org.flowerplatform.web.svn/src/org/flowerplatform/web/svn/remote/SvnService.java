09f8e07cdd0b20c721485ec02627af3a2cc5865d
Gabriela Murgoci <gabriela.murgoci@crispico.com>
2013-08-12 16:28:23 +0300

    SVN: FP commit after sync

---
+++ org.flowerplatform.web.svn/src/org/flowerplatform/web/svn/remote/SvnService.java
@@ -0,0 +1,15 @@
+package org.flowerplatform.web.svn.remote;
+
+
+/**
+ * @flowerModelElementId _bcYyQAMcEeOrJqcAep-lCg
+ */
+public class SvnService {
+	/**
+	 * @flowerModelElementId _yaKVkAMcEeOrJqcAep-lCg
+	 */
+	public void createRemoteFolder(List<PathFragment> parentPath,
+			String folderName, String comment) {
+		// TODO implement
+	}
+}
