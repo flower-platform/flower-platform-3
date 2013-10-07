f0dffe769c25e8366b302fb0f91206e220924686
Tache Razvan <tache.razvan18@gmail.com>
2013-10-07 14:07:24 +0300

    GH21 - Add basic server login : Plugin, Provider, Property, and the interfaces

---
+++ org.flowerplatform.properties/src/org/flowerplatform/properties/Property.java
@@ -0,0 +1,51 @@
+/* license-start
+ * 
+ * Copyright (C) 2008 - 2013 Crispico, <http://www.crispico.com/>.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details, at <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *   Crispico - Initial API and implementation
+ *
+ * license-end
+ */
+package org.flowerplatform.properties;
+/**
+ * @author Tache Razvan Mihai
+ * @return
+ */
+public class Property {
+	
+	private String name;
+	private Object value;
+
+	public Property(String name, Object value) {
+		super();
+		this.name = name;
+		this.value = value;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+}
