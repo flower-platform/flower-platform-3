5532f5c79ac9bf61c5b6898562c6a34d8b8d0b4f
cristian-spiescu <cristian.spiescu@crispico.com>
2013-10-16 19:30:09 +0200

    GH221 - skeleton

---
+++ org.flowerplatform.codesync/src/org/flowerplatform/codesync/CodeSyncElementDescriptor.java
@@ -0,0 +1,16 @@
+package org.flowerplatform.codesync;
+
+import java.util.List;
+
+public class CodeSyncElementDescriptor {
+
+	String codeSyncType;
+	
+	String iconUrl;
+	
+	List<String> codeSyncTypeCategories;
+	
+	List<Object> features;
+	
+	List<String> childrenCodeSyncTypeCategories;
+}
