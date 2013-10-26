dc744729a8325fd101d96734f4f69ee4324bb895
mircea-negreanu <mircea.negreanu@crispico.com>
2013-10-26 14:13:29 +0300

    GH268 - Updated the test script for register new descriptors

--- org.flowerplatform.codesync/scripts/jsScriptTest.js
+++ org.flowerplatform.codesync/scripts/jsScriptTest.js
@@ -3,17 +3,45 @@
 importClass(org.flowerplatform.codesync.remote.CodeSyncElementDescriptor);
 
 var descriptor = new CodeSyncElementDescriptor();
-descriptor.codeSyncType = "backboneClass";
+descriptor.codeSyncType = "backboneClass1";
 descriptor.label = "Backbone Class (js)";
-descriptor.defaultName = "NewBackboxClass";
+descriptor.iconUrl = "images/full/obj16/jcu_obj.gif";
+descriptor.defaultName = "NewBackboxClass (from js)";
 descriptor.extension = "js";
-descriptor.addChildrenCodeSyncTypeCategory("backboneClassMember");
+descriptor.addChildrenCodeSyncTypeCategory("backboneClassMember1");
 descriptor.addChildrenCodeSyncTypeCategory("requiredEntry");
-descriptor.addFeature("superClass");
+descriptor.addFeature("superClass1");
 descriptor.addFeature("name");
 descriptor.keyFeature = "name";
+descriptor.standardDiagramControllerProviderFactory = "topLevelBox";
 
 importClass(com.crispico.flower.mp.codesync.base.CodeSyncPlugin);
+CodeSyncPlugin.instance.codeSyncElementDescriptors.add(descriptor);
+
+descriptor = new CodeSyncElementDescriptor();
+descriptor.codeSyncType = "javaScriptOperation";
+descriptor.label = "Operation";
+descriptor.iconUrl = "images/full/obj16/methpub_obj.gif";
+descriptor.defaultName = "newOperation (from js)";
+descriptor.addCodeSyncTypeCategory("backboneClassMember1");
+descriptor.nextSiblingSeparator = ", ";
+descriptor.addFeature("name");
+descriptor.addFeature("parameters");
+descriptor.keyFeature = "name";
+descriptor.standardDiagramControllerProviderFactory = "topLevelBoxChild";
+CodeSyncPlugin.instance.codeSyncElementDescriptors.add(descriptor);
+
+descriptor = new CodeSyncElementDescriptor();
+descriptor.codeSyncType = "javaScriptAttribute";
+descriptor.label = "Attribute";
+descriptor.iconUrl = "images/full/obj16/field_public_obj.gif";
+descriptor.defaultName = "newAttribute (from js)";
+descriptor.addCodeSyncTypeCategory("backboneClassMember1");
+descriptor.nextSiblingSeparator = ", ";
+descriptor.addFeature("name");
+descriptor.addFeature("defaultValue");
+descriptor.keyFeature = "name";
+descriptor.standardDiagramControllerProviderFactory = "topLevelBoxChild";
 CodeSyncPlugin.instance.codeSyncElementDescriptors.add(descriptor);
 
 // adding a new processor by implementing an interface
@@ -35,9 +63,11 @@ var tablProcessor = new IChangesProcessor() {
 CodeSyncPlugin.instance.codeSyncTypeCriterionDispatcherProcessor.addProcessor("backboneClass", tablProcessor);
 
 // and another processor by extending an existing one
-importClass(org.flowerplatform.codesync.code.javascript.processor.JavascriptElementProcessor);
 importClass(org.flowerplatform.codesync.remote.CodeSyncOperationsService);
 importClass(com.crispico.flower.mp.model.codesync.CodeSyncPackage);
+importClass(org.flowerplatform.editor.model.EditorModelPlugin);
+importClass(org.flowerplatform.codesync.code.javascript.processor.JavascriptElementProcessor);
+
 var proc = new JavaAdapter(JavascriptElementProcessor, {
 	getLabel: function(object, forEditing) {
 		var name = CodeSyncOperationsService.instance
@@ -46,5 +76,5 @@ var proc = new JavaAdapter(JavascriptElementProcessor, {
 	}
 });
 
-importClass(org.flowerplatform.editor.model.EditorModelPlugin);
-EditorModelPlugin.instance.diagramUpdaterChangeProcessor.addDiagrammableElementFeatureChangeProcessor("classDiagram.backboneClass.javaScriptAttribute", proc);
+EditorModelPlugin.instance.diagramUpdaterChangeProcessor.addDiagrammableElementFeatureChangeProcessor("classDiagram.backboneClass1.javaScriptOperation", proc);
+EditorModelPlugin.instance.diagramUpdaterChangeProcessor.addDiagrammableElementFeatureChangeProcessor("classDiagram.backboneClass1.javaScriptAttribute", proc);
